3d438b1f31afce71503a9d613fb67e5f7a686347
Caio Henrique <142325254+HBlackzH@users.noreply.github.com>
2023-09-29 22:22:07 -0300

    Update exercicio4.c

--- exercicio4.c
+++ exercicio4.c
@@ -1,1 +1,30 @@
+#include <stdio.h>
+#include <math.h>
 
+int main(){
+    int ladoA,ladoB,ladoC;
+
+    printf("Digite 3 numeros consecultivos:");
+    scanf("%d %d %d",&ladoA,&ladoB,&ladoC);
+
+if (ladoA + ladoB >ladoC && ladoB + ladoC > ladoA && ladoA + ladoC > ladoB)
+    {
+    printf("é um triangulo");
+
+    if (ladoA == ladoB && ladoA == ladoB && ladoB == ladoC)
+    {printf(" equilatero ");
+
+    }else if (ladoA != ladoB && ladoA != ladoC && ladoB != ladoC)
+    {
+        printf(" escaleno ");
+    }else 
+    {
+        printf(" isósceles ");
+    }
+
+}else{
+printf("não é um triangulo");
+}
+
+return 0;    
+}
