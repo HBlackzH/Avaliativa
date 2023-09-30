12e8e93a017d7b52aea5bd4d3623f52fffcfd4d4
Caio Henrique <142325254+HBlackzH@users.noreply.github.com>
2023-09-29 22:23:02 -0300

    Update exercicio4.c

--- exercicio4.c
+++ exercicio4.c
@@ -2,25 +2,38 @@
 #include <math.h>
 
 int main(){
-    int ladoA,ladoB,ladoC;
+    int lado1,lado2,lado3;
+    int cos1,cos2,cos3;
+    int quad1,quad2,quad3;
 
     printf("Digite 3 numeros consecultivos:");
-    scanf("%d %d %d",&ladoA,&ladoB,&ladoC);
+    scanf("%d %d %d",&lado1,&lado2,&lado3);
 
-if (ladoA + ladoB >ladoC && ladoB + ladoC > ladoA && ladoA + ladoC > ladoB)
+if (lado1 + lado2 >lado3 && lado2 + lado3 > lado1 &&lado1 + lado3>lado2)
     {
     printf("é um triangulo");
 
-    if (ladoA == ladoB && ladoA == ladoB && ladoB == ladoC)
-    {printf(" equilatero ");
+    quad1 = lado1*lado1 ;
+    quad2 = lado2*lado2 ;
+    quad3 = lado3*lado3 ;
 
-    }else if (ladoA != ladoB && ladoA != ladoC && ladoB != ladoC)
-    {
-        printf(" escaleno ");
-    }else 
-    {
-        printf(" isósceles ");
-    }
+    cos1 = (quad1 + quad2 -quad3)/2*lado1*lado2;
+    cos2 = (quad1 + quad3 -quad2)/2*lado1*lado3;
+    cos3 = (quad2 + quad3-quad1)/2*lado2*lado3;
+
+    if (cos1 > 90 || cos2 > 90 || cos3 > 90)
+        {
+            printf("  obtusângulo ");
+        }
+    else if (cos1 ==90 || cos2 == 90 || cos3 == 90)
+        {
+            printf(" retângulo ");
+        }
+    else
+        {
+            printf(" acutângulo ");
+        }
+    
 
 }else{
 printf("não é um triangulo");
